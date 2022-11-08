800a4a55c18c9f36c3721173fcf462ec09c80a4d
AZK <alexanderk@untether.ai>
2022-11-08 14:50:51 -0500

    basic minimax complete

--- c++/main.cpp
+++ c++/main.cpp
@@ -24,7 +24,46 @@ void display_position( thc::ChessRules &cr, const std::string &description )
     printf( "Position = %s\n", s.c_str() );
 }
 
-int minimax(thc::ChessRules &cr, int depth, float alpha, float beta) {
+float boardCalc(thc::ChessRules &cr){
+    std::string fen = cr.ForsythPublish();
+    auto first_token = fen.substr(0, fen.find(' '));
+    float score = 0.0;
+    for(int i = 0; i < first_token.length(); i++){
+        if(fen[i] == 'p'){
+            score -= 1;
+        }
+        else if(fen[i] == 'P'){
+            score += 1;
+        }
+        else if(fen[i] == 'n'){
+            score -= 3;
+        }
+        else if(fen[i] == 'N'){
+            score += 3;
+        }
+        else if(fen[i] == 'b'){
+            score -= 3;
+        }
+        else if(fen[i] == 'B'){
+            score += 3;
+        }
+        else if(fen[i] == 'r'){
+            score -= 5;
+        }
+        else if(fen[i] == 'R'){
+            score += 5;
+        }
+        else if(fen[i] == 'q'){
+            score -= 9;
+        }
+        else if(fen[i] == 'Q'){
+            score += 9;
+        }
+    }
+    return score;
+}
+
+float minimax(thc::ChessRules &cr, int depth, float alpha, float beta) {
     
     thc::TERMINAL terminal;
     cr.Evaluate(terminal);
@@ -41,39 +80,85 @@ int minimax(thc::ChessRules &cr, int depth, float alpha, float beta) {
         return 0;
     }
     
+    float best = 0;
     
     if(cr.WhiteToPlay()){
         //maximizing player
+        best = -100000;
+        std::vector<thc::Move> moves;
+        cr.GenLegalMoveList(moves);
+        unsigned int len = moves.size();
+        for(int i = 0; i < len; i++){
+            cr.PushMove(moves[i]);
+            float score = minimax(cr, depth - 1, alpha, beta);
+            cr.PopMove(moves[i]); 
+            best = std::max(best, score);
+            alpha = std::max(alpha, best);
+            if(beta <= alpha){
+                break;
+            }
+        }
     }
     else{
         //minimizing player
+        best = 100000;
+        std::vector<thc::Move> moves;
+        cr.GenLegalMoveList(moves);
+        unsigned int len = moves.size();
+        for(int i = 0; i < len; i++){
+            cr.PushMove(moves[i]);
+            float score = minimax(cr, depth - 1, alpha, beta);
+            cr.PopMove(moves[i]); 
+            best = std::min(best, score);
+            beta = std::min(beta, best);
+            if(beta <= alpha){
+                break;
+            }
+        }
     }
 
-    return 0;
+    return best;
 }
 
 thc::Move bestMove(thc::ChessRules &cr, int depth) {
     thc::Move mv;
     std::vector<thc::Move> moves;
-    std::vector<bool> check;
-    std::vector<bool> mate;
-    std::vector<bool> stalemate;
-    cr.GenLegalMoveList(moves, check, mate, stalemate);
+    cr.GenLegalMoveList(moves);
     unsigned int len = moves.size();
-    int alpha = -1000000;
-    int beta = 1000000;
+    float alpha = -1000000;
+    float beta = 1000000;
+    float best = 0;
     if(cr.WhiteToPlay()){
+        best = -100000;
         for(int i = 0; i < len; i++){
             cr.PushMove(moves[i]);
-            int score = minimax(cr, depth-1, alpha, beta);
+            float score = minimax(cr, depth-1, alpha, beta);
             cr.PopMove(moves[i]);
+            best = std::max(best, score);
+            alpha = std::max(alpha, best);
+            if(best == score){
+                mv = moves[i];
+            }
+            if(beta <= alpha){
+                break;
+            }
+
         }
     } 
     else {
+        best = 100000;
         for(int i = 0; i < len; i++){
             cr.PushMove(moves[i]);
-            int score = minimax(cr, depth-1, alpha, beta);
+            float score = minimax(cr, depth-1, alpha, beta);
             cr.PopMove(moves[i]);
+            best = std::min(best, score);
+            beta = std::min(beta, best);
+            if(best == score){
+                mv = moves[i];
+            }
+            if(beta <= alpha){
+                break;
+            }
         }
     }
     return mv;
@@ -89,9 +174,11 @@ int main()
     thc::TERMINAL terminal;
     cr.Evaluate(terminal);
     while(terminal == thc::NOT_TERMINAL){
-        thc::Move mv = bestMove(cr, 3);
+        thc::Move mv = bestMove(cr, 6);
         cr.PushMove(mv);
         cr.Evaluate(terminal);
+
+        display_position(cr, "After move");
 
         std::cin >> input;
     }
